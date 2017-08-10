0bcae4940db81443286715cb4a761c0d413b8ed0
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-08-10 10:58:00 -0700

    initial config

--- app.config.js
+++ app.config.js
@@ -1,11 +1,11 @@
 
-appVersion = '1.1.1'
+appVersion = '1.0.0'
 module.exports = {
-    name: 'Demo App',
-    name_long: 'Demo App V' + appVersion,
+    name: 'Resilience',
+    name_long: 'AD Resilience V' + appVersion,
     version: appVersion,
     theme_color: '#3F51B5',
     background_color: "#FF9800",
-    dbPrefix: 'changeMeDev:',
+    dbPrefix: 'adReslience:',
     appType: 'tabs' //options are: tabs|default
 }
